a86541cfe37d179cbdb47289e44f0462d096c16b
Huang Song <hskokoyou@gmail.com>
2025-11-27 17:08:04 +0800

    initial xcode setup

---
+++ HardPlan/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HardPlan
+//
+//  Created by HUANG SONG on 27/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HardPlan/HardPlanApp.swift
@@ -0,0 +1,17 @@
+//
+//  HardPlanApp.swift
+//  HardPlan
+//
+//  Created by HUANG SONG on 27/11/25.
+//
+
+import SwiftUI
+
+@main
+struct HardPlanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
